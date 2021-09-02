4621833300105e279cdd3fc666f0a825ed52c4e1
Ali Akkas <aliakkas006@gmail.com>
2021-09-02 18:01:46 +0600

    error handling

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 // Get DOM element:
 const inputField = document.getElementById('input-field');
+const errorDiv = document.getElementById('errors');
 const displayBookDiv = document.getElementById('display-book');
 const searchResult = document.getElementById('search-result');
 
+// Click handler add:
 document.getElementById('search-btn').addEventListener('click', async () => {
     // console.log('clicked');
     const inputValue = inputField.value;
+    // Search error handle:
+    if (inputValue === '') {
+        errorDiv.innerText = "Search field can't be empty!";
+        return;
+    }
     // console.log(inputValue);
     const response = await fetch( `https://openlibrary.org/search.json?q=${inputValue}` );
     const data = await response.json();
     // console.log(data);
+    inputField.value = '';
     searchResult.innerText = `Related result found: ${data.numFound}`;
     displayBook(data.docs);
 
-    inputField.value = '';
+    
 
 });
 
@@ -27,7 +35,7 @@ const displayBook = books => {
         const div = document.createElement('div');
         div.innerHTML = `
         <h3 class="fs-3">Book name: ${element.title}
-        <p class="fs-6">Author name: ${element.author_name[0]}
+        <p class="fs-6 mb-1">Author name: ${element.author_name[0]}
         <p class="fs-6">First published: ${element.first_publish_year}
     `;
         displayBookDiv.appendChild(div);
